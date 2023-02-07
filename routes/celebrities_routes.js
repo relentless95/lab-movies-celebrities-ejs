36e4483fdafdfb1d127a08e5f2d5a838c6b2066f
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-02-07 08:40:36 +0100

    done until iteration 4

--- routes/celebrities_routes.js
+++ routes/celebrities_routes.js
@@ -1,42 +1,43 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 // importing model
-const celebMod = require('../models/Celebrity.model')
+const celebMod = require("../models/Celebrity.model");
 // all your routes here
 // using the router.get
-router.get('/', async(req, res)=>{
-    res.render('celebrities/celebrities')
-}
-)
-
-
+router.get("/", async (req, res) => {
+  res.render("celebrities/celebrities");
+});
 
 router.get("/create", async (req, res) => {
   try {
-    console.log('celebrities route is working')
+    console.log("celebrities route is working");
     res.render("celebrities/new-celebrity.ejs");
   } catch (error) {
     console.log("there was an error with your route");
   }
 });
 
-router.post("/create", async(req, res)=>{
-    try{
-        const body= req.body
-        const createdCeleb = await celebMod.create({...body})
-    
-        res.redirect('/celebs')
-    } catch (error){
-        console.log("there was an error in the post", error)
-    }
+router.post("/create", async (req, res) => {
+  try {
+    const body = req.body;
+    const createdCeleb = await celebMod.create({ ...body });
 
-})
+    res.redirect("/celebs");
+  } catch (error) {
+    console.log("there was an error in the post", error);
+  }
+});
 
-router.get('/celebs', async(req, res)=>{
-    const findCelebs = celebMod.find()
-    res.render('')
-})
-
-
+// using find
+router.get("/", async (req, res) => {
+  try {
+    const arrCelebs = await celebMod.find();
+    console.log(arrCelebs)
+    console.log('marco')
+    res.render("celebrities", {celebsArr :arrCelebs});
+  } catch (error) {
+    console.log('there was an error using celebMod',error);
+  }
+});
 
 module.exports = router;
